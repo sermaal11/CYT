562331cb65f7cb3d0e796487f2df469c1eaa1dc7
Sergio Marin Alvarez <smarin-a@c2r8s5.42madrid.com>
2023-10-20 18:39:23 +0200

    Bibliotecas estaticas

---
+++ 10_bibliotecas_estaticas/bibliotecas_estaticas.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bibliotecas_estaticas.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smarin-a <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/20 18:20:36 by smarin-a          #+#    #+#             */
+/*   Updated: 2023/10/20 18:38:55 by smarin-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+// Una biblioteca sirve para aportar funciones a otros proyectos que necesiten
+// utilizarlas. Otra de las ventajas de las bibliotecas es que nos permiten
+// distribuir facilmente el codigo que hay dentro de ellas. Las bibliotecas
+// son un pilar fundamental a la hora de realizar software ya que nos van a
+// facilitar el trabajo.
+
+// A la hora de realizar bibliotecas, existen 2 tipos bibliotecas estaticas y
+// bibliotecas dinamicas. En este caso, nos centraremos en la bibliotecas
+// estaticas.
+
+// Una biblioteca estatica, es la forma mas simple de crear una biblioteca.
+// Se trata de un archivo empaquetado, como si de un .zip se tratase y dentro
+// de ese archivo va a estar todo el codigo objeto de nuestra biblioteca.
+//
+// MINUTO 3:45 tutorial c - 37 bibliotecas estaticas makigas
+
+
---
+++ 9_variables_estaticas/variables_global.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   variables_global.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smarin-a <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/20 17:41:21 by smarin-a          #+#    #+#             */
+/*   Updated: 2023/10/20 18:11:21 by smarin-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+// Una variable  global es una variable a la que podremos accder desde
+// cualquier funcion, a diferencia de una variable local, que solo tiene
+// validez dentro de los corchetes {} en la que la variable este contenido.
+
+// Una variable estatica la declaramos en el nivel mas alto, es decir, fuera
+// de una funcion.
+
+// El principal problema de las variable estaticas es que han de estar bien
+// identificadas, porque si no es muy facil perder el origen de la misma.
+// Para ello, cada vez que definamos una variable globlal, la nombraremos con
+// el prefijo g_, --> g_name_variable.
+
+int	g_total = 0;
+int	g_acumulado = 0;
+
+void	asignar(void)
+{
+	acumulado = 25;
+	total = 4;
+}
+
+void	media(void)
+{
+	float	media;
+
+	media = ((float) acumulado) / total;
+	printf("La media es %f.\n", media);
+}
+
+int	main(void)
+{
+	asignar();
+	media();
+	return (0);
+}
